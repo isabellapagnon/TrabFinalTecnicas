d35657619f2981270f8871cc39a2c2a8e49b57d0
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-06-20 16:54:38 -0300

    Creating project with Spring-Boot

---
+++ trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/TrabalhoFinalTecnicasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo13.trabalhoFinalTecnicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalTecnicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalTecnicasApplication.class, args);
+	}
+
+}
