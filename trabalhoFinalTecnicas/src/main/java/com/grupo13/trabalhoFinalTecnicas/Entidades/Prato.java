4592c66b7a75c38ca60d8e27be8e4b907522f078
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-06-21 21:25:19 -0300

    Refactoring Entities implementation and Creating PratoTest file

--- trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/Entidades/Prato.java
+++ trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/Entidades/Prato.java
@@ -4,12 +4,18 @@ public class Prato {
     private String nomePrato;
     private double preco;
 
-    private Prato(String nomePrato, double preco) {
+    public Prato(String nomePrato, double preco) {
         this.nomePrato = nomePrato;
         this.preco = preco;
     }
 
     public static Prato criaNovoPrato(String nomePrato, double preco) {
+        if(preco < 0.0){
+            throw new IllegalArgumentException("Preço inválido");
+        }
+        if(nomePrato == null){
+            throw new IllegalArgumentException("Nome do Prato inválido");
+        }
         return new Prato(nomePrato, preco);
     }
 
