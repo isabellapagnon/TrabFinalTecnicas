121eef5de65c55a4c7930017e098e365d9bf3366
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-06-24 21:46:39 -0300

    ClienteFachadaRemota - initial commit

--- trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/Interfaces/TrabalhoFinalTecnicasApplication.java
+++ trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/Interfaces/TrabalhoFinalTecnicasApplication.java
@@ -1,11 +1,15 @@
-package com.grupo13.trabalhoFinalTecnicas;
+package com.grupo13.trabalhoFinalTecnicas.Interfaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.grupo13"})
+@EnableJpaRepositories(basePackages = {"com.grupo13"})
+@EntityScan(basePackages = {"com.grupo13"})
 public class TrabalhoFinalTecnicasApplication {
 
 	public static void main(String[] args) {
