ad8b2b2e64658c35dbeea87a14ec0acc8d9972ce
Giulia Ferraro Borba <giulia.borba@acad.pucrs.br>
2020-06-27 13:07:24 -0300

    adiciona idPrato e qntd e politicaPedidoMaximo

---
+++ trabalhoFinalTecnicas/src/main/java/com/grupo13/trabalhoFinalTecnicas/CasosDeUso/PoliticaPedidoMaximo.java
@@ -0,0 +1,14 @@
+package com.grupo13.trabalhoFinalTecnicas.CasosDeUso;
+
+import com.grupo13.trabalhoFinalTecnicas.Entidades.Pedidos;
+
+public class PoliticaPedidoMaximo {
+
+    public void verificarPedidoMaximo(Pedidos pedido) {
+        for (int i = 0; i < pedido.getPratos().size(); i++) {
+            if (pedido.getPratos().get(i).getQuantidade() > 5) {
+                throw new IllegalArgumentException("Só é possível pedir 5 unidades de cada prato!");
+            }
+        }
+    }   
+}
